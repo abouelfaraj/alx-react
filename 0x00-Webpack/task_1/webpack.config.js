a9f7b2dd13704752c7de2ac8964cc383d808def4
abouelfaraj <aef.ayoub@gmail.com>
2024-04-02 13:59:33 +0000

    cmt webpack

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: {
     main: path.resolve(__dirname, '/js/dashboard_main.js'),
-  }
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
