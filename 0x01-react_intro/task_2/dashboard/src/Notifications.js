5d50dc21d44f518a272c6935210dd2c7e2e40dbe
abouelfaraj <aef.ayoub@gmail.com>
2024-04-11 14:54:04 -0700

    cmt task 4

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -3,8 +3,19 @@ import './Notifications.css';
 
 function Notifications() {
   return (
-    <div className="">
+    <div className="Notifications">
+      <button
+	style="{{ float: 'right' }}
+	aria-label="Close"
+	onClick={handleClick} >
+      <img src={closeIcon} alt="Close" />
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+	<li data-priority="default">New course available</li>
+	<li data-priority="default">New resume available</li>
+	<li dangerouslySetInnerHTML={{ __html:getLatestNotification() }}></li>
+      </ul>
     </div>
   );
 }
