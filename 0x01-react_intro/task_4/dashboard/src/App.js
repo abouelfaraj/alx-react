a3d955afd3c2c9556bfbf69bc6244e27eaa5f742
abouelfaraj <aef.ayoub@gmail.com>
2024-04-12 14:13:50 -0700

    cmt task 8

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+  return (
+    <div className="App">
+      <header className="App-header">
+	<img src={logo} className="App-logo" alt="Holberton logo" />
+	<h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard></p>
+	<label htmlFor="email">Email:</label> <input type="email" id="email" />
+	<label htmlFor="password">Password:</label> <input type="password" id="password" />
+	<button> OK </button>
+      </div>
+      <footer className="App-footer">
+	<p>{getFooterCopy(true)}></p>
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </footer>
+    </div>     
+  );
+}
+
+export default App;
