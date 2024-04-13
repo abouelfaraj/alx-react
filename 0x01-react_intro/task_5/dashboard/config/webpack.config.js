9fc5b6863041974c37f404de358f21b93bf990df
abouelfaraj <aef.ayoub@gmail.com>
2024-04-13 16:27:18 -0700

    cmt task 5

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
