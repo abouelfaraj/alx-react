d97763a31ae11dd7a235f315975b40a97758554c
abouelfaraj <aef.ayoub@gmail.com>
2024-04-18 02:50:45 -0700

    cmt task 11

---
+++ 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Notifications.css';
+import closeIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
+function Notifications() {
+  const handleClick = () => {
+    console.log('Close button');
+  };
+
+  return (
+    <div className="Notifications">
+      <button
+	style={{ float: 'right' }}
+	aria-label="Close"
+	onClick={handleClick} >
+      <img src={closeIcon} alt="Close" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+	<li data-priority="default">New course available</li>
+	<li data-priority="default">New resume available</li>
+	<li dangerouslySetInnerHTML={{ __html:getLatestNotification() }}></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
