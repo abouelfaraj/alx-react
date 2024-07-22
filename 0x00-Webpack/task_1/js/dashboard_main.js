7b0c46ecf2a61785f5e1eeb832500b11e4b39200
aefayoub <aef.ayoub@gmail.com>
2024-07-22 18:34:10 +0000

    cmt task 1! #

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -6,10 +6,12 @@ let count = 0;
 const updateCounter = _.debounce(() => {
     count++;
     $('#count').text(`${count} clicks on the button`);
-}, 500);
+}, 1000);
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>')
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').click(updateCounter);
