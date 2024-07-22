c485bb5aa911de5b5b3fcf0098b33844e02881b1
aefayoub <aef.ayoub@gmail.com>
2024-07-22 19:05:20 +0000

    cmt task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,18 +3,18 @@ import _ from 'lodash';
 
 let count = 0;
 
-const updateCounter = _.debounce(() => {
-  return () => {
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>')
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+const updateCounter = () => {
+  $('button').on('click', () => {
     count++;
     $('#count').text(`${count} clicks on the button`);
-  };
-}, 1000);
-$(document).ready(() => {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>')
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+  });
+};
 
-  $('button').click(updateCounter);
-});
+_.debounce(updateCounter, 500);
+updateCounter();
