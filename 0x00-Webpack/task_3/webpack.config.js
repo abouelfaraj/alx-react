18143e4b11b724d66742b1381b6fc5eb0971bff4
aefayoub <aef.ayoub@gmail.com>
2024-07-26 20:47:58 +0000

    cmt webpack js

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     devServer: {
-        static: path.join(__dirname, 'public'),
+        static: path.join(__dirname, './public'),
         open: true,
         port: 8564,
     },
