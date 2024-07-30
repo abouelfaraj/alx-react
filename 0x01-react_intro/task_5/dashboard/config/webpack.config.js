4d02d10046e26e70dd9382d0c16c867b4214cd10
aefayoub <aefayoub@gmail.com>
2024-07-30 12:00:37 +0000

    cmt task 9

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,64 +1,56 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './task_5/dashboard/src/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: 'bundle.js',
     },
     devtool: 'inline-source-map',
     devServer: {
-	    static: {
-	      directory: path.resolve(__dirname, '../dist'), // Replaces contentBase
-	    },
-	    hot: true, // Enables Hot Module Replacement
-	    client: {
-	      logging: 'info', // Logging level for the client
-	    },
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
+        static: {
+            directory: path.resolve(__dirname, '../dist'),
         },
+        hot: true, // Enables Hot Module Replacement
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], // Use style-loader for development
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                        },
+                    },
+                ],
+            },
+        ],
     },
     plugins: [
-        new HTMLWebpackPlugin({
-            template: './task_5/dashboard/dist/index.html',
+        new HtmlWebpackPlugin({
+            template: './dist/index.html',
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: '[name].css',
         }),
     ],
-    module: {
-        rules: [
-	      {
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: 'babel-loader',
-	      },
-	      {
-		test: /\.css$/,
-		use: ['style-loader', 'css-loader'],
-	      },
-	      {
-		test: /\.(png|svg|jpg|gif)$/,
-		use: [
-		  {
-		    loader: 'file-loader',
-		    options: {
-		      name: '[name].[ext]',
-		      outputPath: 'images/',
-			    },
-		  },
-		],
-	      },
-    	],
-    },
     performance: {
         maxAssetSize: 1000000,
     },
