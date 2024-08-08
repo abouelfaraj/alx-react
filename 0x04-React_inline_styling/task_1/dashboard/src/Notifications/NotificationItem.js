5f14156ab75894ff410d819a5e033d767159d139
abouelfaraj <156446000+abouelfaraj@users.noreply.github.com>
2024-08-08 17:23:24 +0200

    commit notif item js

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.js
@@ -2,23 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class NotificationItem extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
+    const { type, value, html, markAsRead, id } = this.props;
     return (
-      this.props.value ? 
-      <li
-      data-notification-type={this.props.type}
-      onClick={() => this.props.markAsRead(this.props.id)}
-      >{this.props.value}</li> 
-      :
-      <li
-      data-notification-type={this.props.type}
-      dangerouslySetInnerHTML={this.props.html}
-      onClick={() => {console.log('empty func');}}
-      ></li>
+      <>
+        {type && value ? (
+          <li onClick={() => markAsRead(id)} data-notification-type={type}>
+            {value}
+          </li>
+        ) : null}
+        {html ? <li onClick={() => markAsRead(id)} data-urgent dangerouslySetInnerHTML={{ __html: html }}></li> : null}
+      </>
     );
   }
 }
