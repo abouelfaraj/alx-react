f7fab9e1ec64b9b5a1ef17a7b7eccd45e2d147eb
abouelfaraj <156446000+abouelfaraj@users.noreply.github.com>
2024-08-12 20:40:58 +0200

    commit courseListRow

--- 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,49 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, css } from "aphrodite";
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const [checked, setChecked] = useState(false);
   const headerStyle = { backgroundColor: "#deb5b545" };
   const rowStyle = { backgroundColor: "#f5f5f5ab" };
-  const selectedStyle = isHeader ? headerStyle : rowStyle;
+  const selected_style = isHeader ? headerStyle : rowStyle;
+
+  const handleCheckChange = () => {
+    setChecked(!checked);
+  };
 
   return (
-    <tr style={selectedStyle}>
+    <tr style={selected_style} className={checked ? css(rowsStyles.rowChecked) : ""}>
       {isHeader ? (
         textSecondCell === null ? (
-          <th colSpan="2" className={css(listRowStyles.thcenter)}>{textFirstCell}</th>
+          <th colSpan="2" className={css(rowsStyles.thcenter)}>
+            {textFirstCell}
+          </th>
         ) : (
           <>
-            <th className={css(listRowStyles.th)}>{textFirstCell}</th>
-            <th className={css(listRowStyles.th)}>{textSecondCell}</th>
+            <th className={css(rowsStyles.th)}>{textFirstCell}</th>
+            <th className={css(rowsStyles.th)}>{textSecondCell}</th>
           </>
         )
       ) : (
         <>
-          <td className={css(listRowStyles.td)}>{textFirstCell}</td>
-          <td className={css(listRowStyles.td)}>{textSecondCell}</td>
+          <td className={css(rowsStyles.td)}>
+          <input type="checkbox" onChange={handleCheckChange}/>
+            {textFirstCell}</td>
+          <td className={css(rowsStyles.td)}>{textSecondCell}</td>
         </>
       )}
     </tr>
   );
-};
+}
 
-const listRowStyles = StyleSheet.create({
+const rowsStyles = StyleSheet.create({
   thcenter: {
-    borderBottom: '1px solid gray',
+    borderBottom: "1px solid gray",
     margin: 0,
     padding: 0,
-    textAlign: 'center'
+    textAlign: "center",
   },
   th: {
-    borderBottom: '1px solid gray',
+    borderBottom: "1px solid gray",
     margin: 0,
     padding: 0,
-    textAlign: 'left'
+    textAlign: "left",
   },
   td: {
-    paddingLeft: 3
-  }
+    paddingLeft: 3,
+  },
+  rowChecked: {
+    backgroundColor: "#e6e4e4",
+  },
 });
 
 CourseListRow.defaultProps = {
