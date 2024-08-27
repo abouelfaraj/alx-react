ec4608072362e9bac7b73eb9b9f44c295df9a6e2
root <aef.ayoub@gmail.com>
2024-08-27 20:10:52 +0000

    -remove node directory

--- 0x06-React_Immutable/2-nested.js
+++
@@ -1,6 +0,0 @@
-import { fromJs } from 'immutable';
-
-export default function accessImmutableObject(object, array) {
-    const mappedObj = fromJs(object);
-    return mappedObj.getIn(array, undefined);
-}
